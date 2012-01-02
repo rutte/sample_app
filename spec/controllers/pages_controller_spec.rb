a77b77368d3ceb3c0d2db8568c85ec152b9766c7
Rutger Karlsson <rutger.karlsson@gmail.com>
2012-01-02 23:06:09 +0100

    Added help

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -3,6 +3,10 @@ require 'spec_helper'
 describe PagesController do
 render_views
 
+  before(:each) do
+    @base_title = "RoR Tutorial sample app | "
+  end
+
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
@@ -11,7 +15,7 @@ render_views
     
     it "Should have the right title" do
       get 'home'
-      response.should have_selector("title", :content => "RoR Tutorial sample app | Home")
+      response.should have_selector("title", :content => @base_title + "Home")
     end
   end
 
@@ -23,7 +27,7 @@ render_views
     
     it "Should have the right title" do
       get 'contact'
-      response.should have_selector("title", :content => "RoR Tutorial sample app | Contact")
+      response.should have_selector("title", :content => @base_title + "Contact")
     end
   end
 
@@ -34,7 +38,18 @@ render_views
     end
     it "Should have the right title" do
       get 'about'
-      response.should have_selector("title", :content => "RoR Tutorial sample app | About")
+      response.should have_selector("title", :content => @base_title + "About")
     end
+  end
+  
+    describe "GET 'help'" do
+      it "should be successful" do
+        get 'help'
+        response.should be_success
+      end
+      it "Should have the right title" do
+        get 'help'
+        response.should have_selector("title", :content => @base_title + "Help")
+      end
   end
 end
